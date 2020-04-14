21e058f83f23d0f924c3af8bf36c437d403792fc
wcr000 <riley_wc@yahoo.com>
2020-04-14 17:18:23 -0500

    Initial Commit of Corey Schafer's YouTube VSCode-Python Tutorial

---
+++ script.py
@@ -0,0 +1,21 @@
+
+import math
+import os
+import sys
+
+import requests
+
+print(sys.version)
+print(sys.executable)
+
+
+def greet(who_to_greet):
+    greeting = 'Hello, {}'.format(who_to_greet)
+    return greeting
+
+print(greet('World'))
+# print(greet('Corey'))
+
+
+r = requests.get('https://coreyms.com')
+print(r.status_code)
